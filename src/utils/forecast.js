5e9417f6c663d42642d9b92d84ebcd2dcde3fb1f
JoanaConde <JoanaConde@users.noreply.github.com>
2020-11-21 15:53:16 +0000

    Add about text and add more weather info.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,8 +11,8 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. There is a ' 
-            + body.current.precip +'% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' 
+            + body.current.precip +'% chance of rain. Humidity: ' + body.current.humidity + '%.')
         }
     })
 }
